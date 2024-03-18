fc0223c67e38aadad2d87a32caa6ab1565370f48
topher <97920485+2pher@users.noreply.github.com>
2024-03-18 12:26:16 -0400

    Added some globals

---
+++ globals.h
@@ -0,0 +1,19 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+/* Cyclone V FPGA devices */
+#define LEDR_BASE 0xFF200000
+#define HEX3_HEX0_BASE 0xFF200020
+#define HEX5_HEX4_BASE 0xFF200030
+#define SW_BASE 0xFF200040
+#define KEY_BASE 0xFF200050
+#define TIMER_BASE 0xFF202000
+#define PS2_CONTROLLER 0xFF200100
+#define PIXEL_BUF_CTRL_BASE 0xFF203020
+#define CHAR_BUF_CTRL_BASE 0xFF203030
+
+
+#endif
